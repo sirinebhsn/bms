445cf451fb5fb189dab722cb0990a57a2a2c64b1
syrine ben hassen <syrinebh1996@gmail.com>
2022-02-23 21:34:26 +0100

    update

--- src/views/apps/user/list/Table.js
+++ src/views/apps/user/list/Table.js
@@ -45,7 +45,7 @@ const UsersList = () => {
     setSelectedUser(id)
     setShow(true)
     console.warn(id)
-    let result = await fetch("http://localhost:8000/api/getOwner/" + id);
+    let result = await fetch("https://bmsback.herokuapp.com/api/getOwner/" + id);
     result = await result.json();
     console.warn(result)
 
@@ -68,13 +68,13 @@ const UsersList = () => {
     getData();
   }, [])
   async function getData() {
-    let result = await fetch("http://localhost:8000/api/listOwner");
+    let result = await fetch("https://bmsback.herokuapp.com/api/listOwner");
     result = await result.json();
     setData(result)
   }
 
   async function deleteOperation(id) {
-    let result = await fetch("http://localhost:8000/api/delete/" + id, {
+    let result = await fetch("https://bmsback.herokuapp.com/api/delete/" + id, {
       method: "DELETE"
 
     });
@@ -105,7 +105,7 @@ const UsersList = () => {
   async function search(key) {
     if(key){
     console.warn(key)
-    let result = await fetch("http://localhost:8000/api/search/" + key);
+    let result = await fetch("https://bmsback.herokuapp.com/api/search/" + key);
     result = await result.json();
     console.warn(result)
     setData(result)
